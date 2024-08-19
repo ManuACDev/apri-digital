3d560be3f4e5e3956263b65dff0b4fae4fa4cac9
ManuACDev <malamocres@gmail.com>
2024-08-19 23:53:09 +0200

    Inicializar Header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  toggleMenu() {
+    const sideMenu = document.getElementById('sideMenu');
+    if (sideMenu) {
+      sideMenu.classList.toggle('open');
+    }
+  }
+
 }
