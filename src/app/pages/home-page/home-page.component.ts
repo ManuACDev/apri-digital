bb647815009e3b05fe54370f0471e835d0898973
ManuACDev <malamocres@gmail.com>
2024-08-26 20:49:06 +0200

    Services HTML y CSS v1

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { SliderComponent } from '../../components/slider/slider.component';
+import { ServicesComponent } from "../../components/services/services.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HeaderComponent, SliderComponent],
+  imports: [HeaderComponent, SliderComponent, ServicesComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
