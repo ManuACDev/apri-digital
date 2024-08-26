8b11fb796a278ff17e6b873b73f4457889b47287
ManuACDev <malamocres@gmail.com>
2024-08-26 20:06:36 +0200

    Pages Folder

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
-import { SliderComponent } from "../slider/slider.component";
+import { HeaderComponent } from '../../components/header/header.component';
+import { SliderComponent } from '../../components/slider/slider.component';
 
 @Component({
   selector: 'app-home-page',
