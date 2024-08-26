ec2d81c4f49bed4098393c4e4b9df2227c36abab
ManuACDev <malamocres@gmail.com>
2024-08-26 19:32:19 +0200

    Home Page

---
+++ src/app/components/home-page/home-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { SliderComponent } from "../slider/slider.component";
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [HeaderComponent, SliderComponent],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.css'
+})
+export class HomePageComponent {
+
+}
