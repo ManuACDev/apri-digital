5e87992dcce64bc1f3eac244f00f46417c76baef
ManuACDev <malamocres@gmail.com>
2024-08-27 20:35:53 +0200

    Parallax HTML Y CSS v1

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { SliderComponent } from '../../components/slider/slider.component';
 import { ServicesComponent } from "../../components/services/services.component";
+import { ParallaxComponent } from "../../components/parallax/parallax.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HeaderComponent, SliderComponent, ServicesComponent],
+  imports: [HeaderComponent, SliderComponent, ServicesComponent, ParallaxComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
