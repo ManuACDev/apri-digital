f644ff77997195d646e01ddf19ad67e95f1ccda0
ManuACDev <malamocres@gmail.com>
2024-08-27 18:52:58 +0200

    Router Link Services

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
