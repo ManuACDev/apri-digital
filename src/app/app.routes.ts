cd69d2ec4d64f4d7d00d70816763dd964531acdf
ManuACDev <malamocres@gmail.com>
2024-08-29 12:03:37 +0200

    Router Link Inicio Y Servicios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ServicesPageComponent } from './pages/services-page/services-page.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomePageComponent },
-    { path: '', redirectTo: '/home', pathMatch: 'full' }
+    { path: 'services', component: ServicesPageComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', redirectTo: '/home' }
 ];
