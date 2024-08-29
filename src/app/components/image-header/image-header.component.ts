eac6be99fc0b397b5614db001601031839ea7838
ManuACDev <malamocres@gmail.com>
2024-08-29 11:35:42 +0200

    Image Header Component

---
+++ src/app/components/image-header/image-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './image-header.component.html',
+  styleUrl: './image-header.component.css'
+})
+export class ImageHeaderComponent {
+
+}
