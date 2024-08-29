9fc5ec9a4f60960771a71da004436bab683c0c4f
ManuACDev <malamocres@gmail.com>
2024-08-29 12:54:51 +0200

    HTML Y CSS Tittle v1

--- src/app/pages/services-page/services-page.component.ts
+++ src/app/pages/services-page/services-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from "../../components/title/title.component";
+import { SectionComponent } from "../../components/section/section.component";
 
 @Component({
   selector: 'app-services-page',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent, SectionComponent],
   templateUrl: './services-page.component.html',
   styleUrl: './services-page.component.css'
 })
