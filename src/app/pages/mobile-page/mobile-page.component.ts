51d828bbc2ba8615f0e407a39d58b8f0b3423939
ManuACDev <malamocres@gmail.com>
2024-08-30 20:12:57 +0200

    Mobile Page Section

--- src/app/pages/mobile-page/mobile-page.component.ts
+++ src/app/pages/mobile-page/mobile-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TitleComponent } from "../../components/title/title.component";
+import { SectionComponent } from "../../components/section/section.component";
 
 @Component({
   selector: 'app-mobile-page',
   standalone: true,
-  imports: [TitleComponent],
+  imports: [TitleComponent, SectionComponent],
   templateUrl: './mobile-page.component.html',
   styleUrl: './mobile-page.component.css'
 })
