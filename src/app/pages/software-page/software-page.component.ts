707e01ce65dd21994a3095e762d46b51214739a5
ManuACDev <malamocres@gmail.com>
2024-08-30 20:19:30 +0200

    Software Page Section

--- src/app/pages/software-page/software-page.component.ts
+++ src/app/pages/software-page/software-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TitleComponent } from "../../components/title/title.component";
+import { SectionComponent } from "../../components/section/section.component";
 
 @Component({
   selector: 'app-software-page',
   standalone: true,
-  imports: [TitleComponent],
+  imports: [TitleComponent, SectionComponent],
   templateUrl: './software-page.component.html',
   styleUrl: './software-page.component.css'
 })
