f7ce7acd2652a2177c4b399beaf063f52ff1c8e8
ManuACDev <malamocres@gmail.com>
2024-08-30 09:55:17 +0200

    Desarrollo Software Link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ServicesPageComponent } from './pages/services-page/services-page.component';
+import { SoftwarePageComponent } from './pages/software-page/software-page.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomePageComponent },
     { path: 'services', component: ServicesPageComponent },
+    { path: 'desarrollo', component: SoftwarePageComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', redirectTo: '/home' }
 ];
