04e3b1d5af234038083f0df78d77ebd2a15aaaef
ManuACDev <malamocres@gmail.com>
2024-08-30 14:03:43 +0200

    Section Modular v1

--- src/app/components/section/section.component.ts
+++ src/app/components/section/section.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-section',
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './section.component.css'
 })
 export class SectionComponent {
+
+  @Input() imageSrc: string = "";
+  @Input() mainTitle: string = "";
+  @Input() subTitle: string = "";
+  @Input() paragraph1: string = "";
+  @Input() paragraph2: string = "";
 
 }
--- src/app/pages/consultoria-page/consultoria-page.component.ts
+++ src/app/pages/consultoria-page/consultoria-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TitleComponent } from "../../components/title/title.component";
+import { SectionComponent } from "../../components/section/section.component";
 
 @Component({
   selector: 'app-consultoria-page',
   standalone: true,
-  imports: [TitleComponent],
+  imports: [TitleComponent, SectionComponent],
   templateUrl: './consultoria-page.component.html',
   styleUrl: './consultoria-page.component.css'
 })
