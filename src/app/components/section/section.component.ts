ea0a27b2ccb584a877e07e009c886eb9035be7e7
ManuACDev <malamocres@gmail.com>
2024-08-30 20:02:36 +0200

    Section New Y Consultoria Page

--- src/app/components/section/section.component.ts
+++ src/app/components/section/section.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-section',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './section.component.html',
   styleUrl: './section.component.css'
 })
@@ -11,8 +12,12 @@ export class SectionComponent {
 
   @Input() imageSrc: string = "";
   @Input() mainTitle: string = "";
-  @Input() subTitle: string = "";
-  @Input() paragraph1: string = "";
-  @Input() paragraph2: string = "";
+
+  // Definimos una estructura para secciones con título, contenido y listas opcionales
+  @Input() sections: { 
+    title: string; 
+    content: string; 
+    list?: string[]; 
+  }[] = [];
 
 }
