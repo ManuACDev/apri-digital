ebcb67e47a79405bbf8c7e658c73e7cd340453e1
ManuACDev <malamocres@gmail.com>
2024-08-30 12:01:29 +0200

    Mobile Page Link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ServicesPageComponent } from './pages/services-page/services-page.component';
 import { SoftwarePageComponent } from './pages/software-page/software-page.component';
+import { MobilePageComponent } from './pages/mobile-page/mobile-page.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomePageComponent },
     { path: 'services', component: ServicesPageComponent },
     { path: 'desarrollo', component: SoftwarePageComponent },
+    { path: 'mobile', component: MobilePageComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', redirectTo: '/home' }
 ];
