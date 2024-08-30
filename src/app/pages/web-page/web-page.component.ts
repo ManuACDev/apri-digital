c935211d0f25711d701c4b192eee4e1d1fd61941
ManuACDev <malamocres@gmail.com>
2024-08-30 12:27:58 +0200

    Web Page Import Title

--- src/app/pages/web-page/web-page.component.ts
+++ src/app/pages/web-page/web-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from "../../components/title/title.component";
 
 @Component({
   selector: 'app-web-page',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent],
   templateUrl: './web-page.component.html',
   styleUrl: './web-page.component.css'
 })
