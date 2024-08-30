75a0c1da27ef84a3a43e9e6de77ab94a0d1e9f4a
ManuACDev <malamocres@gmail.com>
2024-08-30 12:12:02 +0200

    Mobile Page Import Title

--- src/app/pages/mobile-page/mobile-page.component.ts
+++ src/app/pages/mobile-page/mobile-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from "../../components/title/title.component";
 
 @Component({
   selector: 'app-mobile-page',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent],
   templateUrl: './mobile-page.component.html',
   styleUrl: './mobile-page.component.css'
 })
