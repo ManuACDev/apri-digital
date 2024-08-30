3f25f70decd397df419b15c98e987a36e2f34e71
ManuACDev <malamocres@gmail.com>
2024-08-30 20:34:58 +0200

    Web Page Section

--- src/app/pages/web-page/web-page.component.ts
+++ src/app/pages/web-page/web-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TitleComponent } from "../../components/title/title.component";
+import { SectionComponent } from "../../components/section/section.component";
 
 @Component({
   selector: 'app-web-page',
   standalone: true,
-  imports: [TitleComponent],
+  imports: [TitleComponent, SectionComponent],
   templateUrl: './web-page.component.html',
   styleUrl: './web-page.component.css'
 })
