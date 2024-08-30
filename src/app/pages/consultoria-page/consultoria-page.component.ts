0b922e1f8b2c9bc94dfe0b9a747b29b24e50beef
ManuACDev <malamocres@gmail.com>
2024-08-30 12:55:49 +0200

    Consultoria Page Import Title

--- src/app/pages/consultoria-page/consultoria-page.component.ts
+++ src/app/pages/consultoria-page/consultoria-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from "../../components/title/title.component";
 
 @Component({
   selector: 'app-consultoria-page',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent],
   templateUrl: './consultoria-page.component.html',
   styleUrl: './consultoria-page.component.css'
 })
