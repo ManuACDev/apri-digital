27a14479deb60c95ff7d2d0b5cfa10773037253e
ManuACDev <malamocres@gmail.com>
2024-08-30 12:32:39 +0200

    Consultoria Page

---
+++ src/app/pages/consultoria-page/consultoria-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consultoria-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './consultoria-page.component.html',
+  styleUrl: './consultoria-page.component.css'
+})
+export class ConsultoriaPageComponent {
+
+}
