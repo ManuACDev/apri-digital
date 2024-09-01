94f7d9ab3897350a7849dd4433d7091dc3368725
ManuACDev <malamocres@gmail.com>
2024-09-01 21:00:13 +0200

    Products Route Y Link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { SoftwarePageComponent } from './pages/software-page/software-page.compo
 import { MobilePageComponent } from './pages/mobile-page/mobile-page.component';
 import { WebPageComponent } from './pages/web-page/web-page.component';
 import { ConsultoriaPageComponent } from './pages/consultoria-page/consultoria-page.component';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomePageComponent },
@@ -13,6 +14,7 @@ export const routes: Routes = [
     { path: 'services/mobile', component: MobilePageComponent },
     { path: 'services/web', component: WebPageComponent },
     { path: 'services/consultoria', component: ConsultoriaPageComponent },
+    { path: 'productos', component: ProductsPageComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', redirectTo: '/home' }
 ];
