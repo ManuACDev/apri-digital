d6726c717ad1a1d6092a2b7cc56e9afabd7f7f29
ManuACDev <malamocres@gmail.com>
2024-09-01 21:31:49 +0200

    Products Page Title

--- src/app/pages/products-page/products-page.component.ts
+++ src/app/pages/products-page/products-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from "../../components/title/title.component";
 
 @Component({
   selector: 'app-products-page',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent],
   templateUrl: './products-page.component.html',
   styleUrl: './products-page.component.css'
 })
