f6e6901d722b970206a0e3a2facdcc0712f384d3
ManuACDev <malamocres@gmail.com>
2024-09-01 23:07:15 +0200

    Products Page Y Card HTML Y CSS v1

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
@@ -12,7 +13,8 @@ export class CardComponent {
   @Input() product!: { 
     title: string; 
     description: string; 
-    imageSrc: string 
+    imageSrc: string;
+    link: string; 
   };
 
 }
--- src/app/pages/products-page/products-page.component.ts
+++ src/app/pages/products-page/products-page.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { TitleComponent } from "../../components/title/title.component";
+import { CardComponent } from "../../components/card/card.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products-page',
   standalone: true,
-  imports: [TitleComponent],
+  imports: [TitleComponent, CardComponent, CommonModule],
   templateUrl: './products-page.component.html',
   styleUrl: './products-page.component.css'
 })
 export class ProductsPageComponent {
+
+  products = [
+    {
+      title: "Producto 1",
+      description: "Descripción breve del producto 1.",
+      imageSrc: "assets/products/padel-group.jpg",
+      link: "/products/product1"  // Enlace a la página del producto específico
+    },
+    {
+      title: "Producto 2",
+      description: "Descripción breve del producto 2.",
+      imageSrc: "assets/products/padel-group.jpg",
+      link: "/products/product2"  // Enlace a la página del producto específico
+    },
+    {
+      title: "Producto 3",
+      description: "Descripción breve del producto 3.",
+      imageSrc: "assets/products/padel-group.jpg",
+      link: "/products/product3"  // Enlace a la página del producto específico
+    },
+    {
+      title: "Producto 4",
+      description: "Descripción breve del producto 4.",
+      imageSrc: "assets/products/padel-group.jpg",
+      link: "/products/product4"  // Enlace a la página del producto específico
+    }
+  ];
 
 }
