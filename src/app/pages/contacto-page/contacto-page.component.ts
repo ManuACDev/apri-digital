7b88deb20cec1a6a27a46c24a11b34fd91f1203c
ManuACDev <malamocres@gmail.com>
2024-09-11 23:02:00 +0200

    Contacto Page

---
+++ src/app/pages/contacto-page/contacto-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contacto-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './contacto-page.component.html',
+  styleUrl: './contacto-page.component.css'
+})
+export class ContactoPageComponent {
+
+}
