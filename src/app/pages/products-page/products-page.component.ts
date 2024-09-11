d107379d7fcef48035ba744dd0906ab377bb87aa
ManuACDev <malamocres@gmail.com>
2024-09-11 19:24:19 +0200

    Assets Products Y Projects

--- src/app/pages/products-page/products-page.component.ts
+++ src/app/pages/products-page/products-page.component.ts
@@ -18,72 +18,9 @@ export class ProductsPageComponent {
       type: "productos",
       title: "TrackManager",
       subtitle: "App móvil para gestionar reservas de pistas deportivas e instalaciones recreativas. Adaptable a todo tipo de reservas.",
-      imageSrc: "assets/products/padel-group.jpg",
-      images: ["assets/products/padel-group.jpg", "assets/products/desarrollo-mobile.jpg", "assets/products/desarrollo-software.jpg", "assets/products/padel-group.jpg", "assets/products/desarrollo-mobile.jpg", "assets/products/desarrollo-software.jpg"],
-      videoUrl: "assets/products/padel-group.mp4",
-      sections: [
-        {
-          title: "Descripción general",
-          content: "TrackManager es una app móvil para la gestión de reservas de cualquier tipo de pistas deportivas o instalaciones recreativas. Con una interfaz intuitiva y funcionalidades avanzadas, permite a los centros deportivos gestionar fácilmente sus horarios, reservas y pagos, proporcionando una experiencia eficiente tanto para administradores como para usuarios. Compatible con múltiples tipos de instalaciones, TrackManager optimiza la operación diaria y mejora la satisfacción del cliente.",
-          list: ["Reserva de pistas", "Integración con sistemas de pago", "Catálogo de productos", "Contacto, información y ubicación"]
-        },
-        {
-          title: "CRM incorporado",
-          content: "Optimiza la gestión de tu centro deportivo con herramientas avanzadas que facilitan la administración diaria, mejoran la comunicación con los clientes y aumentan la eficiencia operativa.",
-          list: ["Gestión de reservas en tiempo real", "Seguimiento y administración de usuarios", "Asignación y gestión de pistas"]
-        }
-      ]
-    },
-    {
-      id: "track-manager",
-      type: "productos",
-      title: "TrackManager",
-      subtitle: "App móvil para gestionar reservas de pistas deportivas e instalaciones recreativas. Adaptable a todo tipo de reservas.",
-      imageSrc: "assets/products/padel-group.jpg",
-      images: ["assets/products/padel-group.jpg", "assets/products/padel-group.jpg", "assets/products/padel-group.jpg"],
-      videoUrl: "assets/products/padel-group.mp4",
-      sections: [
-        {
-          title: "Descripción general",
-          content: "TrackManager es una app móvil para la gestión de reservas de cualquier tipo de pistas deportivas o instalaciones recreativas. Con una interfaz intuitiva y funcionalidades avanzadas, permite a los centros deportivos gestionar fácilmente sus horarios, reservas y pagos, proporcionando una experiencia eficiente tanto para administradores como para usuarios. Compatible con múltiples tipos de instalaciones, TrackManager optimiza la operación diaria y mejora la satisfacción del cliente.",
-          list: ["Reserva de pistas", "Integración con sistemas de pago", "Catálogo de productos", "Contacto, información y ubicación"]
-        },
-        {
-          title: "CRM incorporado",
-          content: "Optimiza la gestión de tu centro deportivo con herramientas avanzadas que facilitan la administración diaria, mejoran la comunicación con los clientes y aumentan la eficiencia operativa.",
-          list: ["Gestión de reservas en tiempo real", "Seguimiento y administración de usuarios", "Asignación y gestión de pistas"]
-        }
-      ]
-    },
-    {
-      id: "track-manager",
-      type: "productos",
-      title: "TrackManager",
-      subtitle: "App móvil para gestionar reservas de pistas deportivas e instalaciones recreativas. Adaptable a todo tipo de reservas.",
-      imageSrc: "assets/products/padel-group.jpg",
-      images: ["assets/products/padel-group.jpg", "assets/products/padel-group.jpg", "assets/products/padel-group.jpg"],
-      videoUrl: "assets/products/padel-group.mp4",
-      sections: [
-        {
-          title: "Descripción general",
-          content: "TrackManager es una app móvil para la gestión de reservas de cualquier tipo de pistas deportivas o instalaciones recreativas. Con una interfaz intuitiva y funcionalidades avanzadas, permite a los centros deportivos gestionar fácilmente sus horarios, reservas y pagos, proporcionando una experiencia eficiente tanto para administradores como para usuarios. Compatible con múltiples tipos de instalaciones, TrackManager optimiza la operación diaria y mejora la satisfacción del cliente.",
-          list: ["Reserva de pistas", "Integración con sistemas de pago", "Catálogo de productos", "Contacto, información y ubicación"]
-        },
-        {
-          title: "CRM incorporado",
-          content: "Optimiza la gestión de tu centro deportivo con herramientas avanzadas que facilitan la administración diaria, mejoran la comunicación con los clientes y aumentan la eficiencia operativa.",
-          list: ["Gestión de reservas en tiempo real", "Seguimiento y administración de usuarios", "Asignación y gestión de pistas"]
-        }
-      ]
-    },
-    {
-      id: "track-manager",
-      type: "productos",
-      title: "TrackManager",
-      subtitle: "App móvil para gestionar reservas de pistas deportivas e instalaciones recreativas. Adaptable a todo tipo de reservas.",
-      imageSrc: "assets/products/padel-group.jpg",
-      images: ["assets/products/padel-group.jpg", "assets/products/padel-group.jpg", "assets/products/padel-group.jpg"],
-      videoUrl: "assets/products/padel-group.mp4",
+      imageSrc: "assets/products/track-manager/1-track-manager.png",
+      images: ["assets/products/track-manager/2-inicio-menu.png", "assets/products/track-manager/3-pistas-disponibles.png", "assets/products/track-manager/4-horario-pistas.png", "assets/products/track-manager/5-pago-stripe.png", "assets/products/track-manager/6-catalogo-productos.png", "assets/products/track-manager/7-contacto-servicios.png", "assets/products/track-manager/8-perfil-admin.png", "assets/products/track-manager/9-gestion-usuarios.png", "assets/products/track-manager/10-gestion-reservas.png", "assets/products/track-manager/11-gestion-pistas.png", "assets/products/track-manager/12-gestion-recibos.png", "assets/products/track-manager/13-gestion-reembolsos.png", "assets/products/track-manager/14-gestion-productos.png", "assets/products/track-manager/15-apri-digital.png"],
+      videoUrl: "assets/products/track-manager/video/track-manager.mp4",
       sections: [
         {
           title: "Descripción general",
--- src/app/pages/projects-page/projects-page.component.ts
+++ src/app/pages/projects-page/projects-page.component.ts
@@ -14,86 +14,23 @@ export class ProjectsPageComponent {
 
   projects = [
     {
-      id: "track-manager",
+      id: "padel-center",
       type: "proyectos",
-      title: "TrackManager",
-      subtitle: "App móvil para gestionar reservas de pistas deportivas e instalaciones recreativas. Adaptable a todo tipo de reservas.",
-      imageSrc: "assets/products/padel-group.jpg",
-      images: ["assets/products/padel-group.jpg", "assets/products/desarrollo-mobile.jpg", "assets/products/desarrollo-software.jpg", "assets/products/padel-group.jpg", "assets/products/desarrollo-mobile.jpg", "assets/products/desarrollo-software.jpg"],
-      videoUrl: "assets/products/padel-group.mp4",
+      title: "Padel Center X4",
+      subtitle: "Implementación de nuestro producto 'TrackManager' para optimizar la gestión de reservas en pistas de pádel.",
+      imageSrc: "assets/projects/padel-center/1-padel-center.png",
+      images: ["assets/projects/padel-center/0-login-registro.png", "assets/projects/padel-center/2-horario-pistas.png", "assets/projects/padel-center/3-pago-stripe.png", "assets/projects/padel-center/4-catalogo-productos.png", "assets/projects/padel-center/5-contacto.png", "assets/projects/padel-center/6-perfil-usuario.png", "assets/projects/padel-center/7-perfil-admin.png", "assets/projects/padel-center/8-gestion-usuario.png", "assets/projects/padel-center/9-gestion-reservas.png", "assets/projects/padel-center/10-gestion-pistas.png", "assets/projects/padel-center/11-gestion-pagos.png", "assets/projects/padel-center/12-gestion-reembolsos.png", "assets/projects/padel-center/13-gestion-productos.png"],
+      videoUrl: "assets/projects/padel-center/video/padel-center.mp4",
       sections: [
         {
-          title: "Descripción general",
-          content: "TrackManager es una app móvil para la gestión de reservas de cualquier tipo de pistas deportivas o instalaciones recreativas. Con una interfaz intuitiva y funcionalidades avanzadas, permite a los centros deportivos gestionar fácilmente sus horarios, reservas y pagos, proporcionando una experiencia eficiente tanto para administradores como para usuarios. Compatible con múltiples tipos de instalaciones, TrackManager optimiza la operación diaria y mejora la satisfacción del cliente.",
-          list: ["Reserva de pistas", "Integración con sistemas de pago", "Catálogo de productos", "Contacto, información y ubicación"]
+          title: "Descripción del Proyecto",
+          content: "Padel Center X4 es una empresa que ofrece instalaciones para la práctica del pádel. Para mejorar su servicio y optimizar la gestión de sus reservas, Padel Center X4 ha integrado la solución 'TrackManager', una app móvil avanzada que permite gestionar fácilmente las reservas de pistas, la comunicación con los clientes, y la administración de sus instalaciones.",
+          list: ["Solución digital para gestión de reservas de pistas de pádel", "Integración con sistemas de pago seguros y eficientes", "Optimización del servicio al cliente y la operativa diaria"]
         },
         {
-          title: "CRM incorporado",
-          content: "Optimiza la gestión de tu centro deportivo con herramientas avanzadas que facilitan la administración diaria, mejoran la comunicación con los clientes y aumentan la eficiencia operativa.",
-          list: ["Gestión de reservas en tiempo real", "Seguimiento y administración de usuarios", "Asignación y gestión de pistas"]
-        }
-      ]
-    },
-    {
-      id: "track-manager",
-      type: "proyectos",
-      title: "TrackManager",
-      subtitle: "App móvil para gestionar reservas de pistas deportivas e instalaciones recreativas. Adaptable a todo tipo de reservas.",
-      imageSrc: "assets/products/padel-group.jpg",
-      images: ["assets/products/padel-group.jpg", "assets/products/padel-group.jpg", "assets/products/padel-group.jpg"],
-      videoUrl: "assets/products/padel-group.mp4",
-      sections: [
-        {
-          title: "Descripción general",
-          content: "TrackManager es una app móvil para la gestión de reservas de cualquier tipo de pistas deportivas o instalaciones recreativas. Con una interfaz intuitiva y funcionalidades avanzadas, permite a los centros deportivos gestionar fácilmente sus horarios, reservas y pagos, proporcionando una experiencia eficiente tanto para administradores como para usuarios. Compatible con múltiples tipos de instalaciones, TrackManager optimiza la operación diaria y mejora la satisfacción del cliente.",
-          list: ["Reserva de pistas", "Integración con sistemas de pago", "Catálogo de productos", "Contacto, información y ubicación"]
-        },
-        {
-          title: "CRM incorporado",
-          content: "Optimiza la gestión de tu centro deportivo con herramientas avanzadas que facilitan la administración diaria, mejoran la comunicación con los clientes y aumentan la eficiencia operativa.",
-          list: ["Gestión de reservas en tiempo real", "Seguimiento y administración de usuarios", "Asignación y gestión de pistas"]
-        }
-      ]
-    },
-    {
-      id: "track-manager",
-      type: "proyectos",
-      title: "TrackManager",
-      subtitle: "App móvil para gestionar reservas de pistas deportivas e instalaciones recreativas. Adaptable a todo tipo de reservas.",
-      imageSrc: "assets/products/padel-group.jpg",
-      images: ["assets/products/padel-group.jpg", "assets/products/padel-group.jpg", "assets/products/padel-group.jpg"],
-      videoUrl: "assets/products/padel-group.mp4",
-      sections: [
-        {
-          title: "Descripción general",
-          content: "TrackManager es una app móvil para la gestión de reservas de cualquier tipo de pistas deportivas o instalaciones recreativas. Con una interfaz intuitiva y funcionalidades avanzadas, permite a los centros deportivos gestionar fácilmente sus horarios, reservas y pagos, proporcionando una experiencia eficiente tanto para administradores como para usuarios. Compatible con múltiples tipos de instalaciones, TrackManager optimiza la operación diaria y mejora la satisfacción del cliente.",
-          list: ["Reserva de pistas", "Integración con sistemas de pago", "Catálogo de productos", "Contacto, información y ubicación"]
-        },
-        {
-          title: "CRM incorporado",
-          content: "Optimiza la gestión de tu centro deportivo con herramientas avanzadas que facilitan la administración diaria, mejoran la comunicación con los clientes y aumentan la eficiencia operativa.",
-          list: ["Gestión de reservas en tiempo real", "Seguimiento y administración de usuarios", "Asignación y gestión de pistas"]
-        }
-      ]
-    },
-    {
-      id: "track-manager",
-      type: "proyectos",
-      title: "TrackManager",
-      subtitle: "App móvil para gestionar reservas de pistas deportivas e instalaciones recreativas. Adaptable a todo tipo de reservas.",
-      imageSrc: "assets/products/padel-group.jpg",
-      images: ["assets/products/padel-group.jpg", "assets/products/padel-group.jpg", "assets/products/padel-group.jpg"],
-      videoUrl: "assets/products/padel-group.mp4",
-      sections: [
-        {
-          title: "Descripción general",
-          content: "TrackManager es una app móvil para la gestión de reservas de cualquier tipo de pistas deportivas o instalaciones recreativas. Con una interfaz intuitiva y funcionalidades avanzadas, permite a los centros deportivos gestionar fácilmente sus horarios, reservas y pagos, proporcionando una experiencia eficiente tanto para administradores como para usuarios. Compatible con múltiples tipos de instalaciones, TrackManager optimiza la operación diaria y mejora la satisfacción del cliente.",
-          list: ["Reserva de pistas", "Integración con sistemas de pago", "Catálogo de productos", "Contacto, información y ubicación"]
-        },
-        {
-          title: "CRM incorporado",
-          content: "Optimiza la gestión de tu centro deportivo con herramientas avanzadas que facilitan la administración diaria, mejoran la comunicación con los clientes y aumentan la eficiencia operativa.",
-          list: ["Gestión de reservas en tiempo real", "Seguimiento y administración de usuarios", "Asignación y gestión de pistas"]
+          title: "Resultados y Beneficios",
+          content: "La implementación de 'TrackManager' ha permitido a Padel Center X4 incrementar la eficiencia operativa y mejorar la experiencia del cliente. Con funcionalidades como la reserva en tiempo real y la gestión centralizada de usuarios, Padel Center X4 ha visto una notable mejora en la organización de su agenda y la satisfacción de sus usuarios.",
+          list: ["Gestión de reservas en tiempo real", "Seguimiento y administración de usuarios", "Reducción de tiempos de administración"]
         }
       ]
     }
