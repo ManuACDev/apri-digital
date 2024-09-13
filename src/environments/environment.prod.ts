a027bb18872ebf134f5aca9ef68b4c2f67eaa47c
ManuACDev <malamocres@gmail.com>
2024-09-13 22:43:30 +0200

    Inicializar Firebase

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const enviroment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAlF02UMi7iXrgQFzdUofzYSup5LkIenW4",
+    authDomain: "apridigital-47fe0.firebaseapp.com",
+    projectId: "apridigital-47fe0",
+    storageBucket: "apridigital-47fe0.appspot.com",
+    measurementId: "G-MYD2XYBJ89",
+    messagingSenderId: "630385607046",
+    appId: "1:630385607046:web:01ff175adcf00f29dcec07"
+  }
+};
