e724657c7a94e3eed022223c6a3de5f70e50d5c7
ManuACDev <malamocres@gmail.com>
2024-09-18 14:01:16 +0200

    Form HTML Y CSS v1

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Entity } from '../../models/entity';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
 
+  @Input() entity: Entity | null = null;
+  @Input() entityType!: string;
+  @Output() submitForm = new EventEmitter<Entity>();
+
+  entityForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.entityForm = this.fb.group({
+      title: ['', Validators.required],
+      subtitle: ['', Validators.required],
+      imageSrc: ['', Validators.required],
+      videoUrl: [''],
+      sections: this.fb.array([])
+    });
+  }
+
+  ngOnInit() {
+    if (this.entity) {
+      this.entityForm.patchValue(this.entity);
+    }
+  }
+
+  onSubmit() {
+    if (this.entityForm.valid) {
+      this.submitForm.emit(this.entityForm.value);
+    }
+  }
+  
 }
