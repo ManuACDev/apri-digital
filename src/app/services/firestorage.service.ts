1d9ec4ce824fcddfba8ccb253fc0ed86ff02f187
ManuACDev <malamocres@gmail.com>
2024-09-19 16:15:23 +0200

    Storage uploadMedia

--- src/app/services/firestorage.service.ts
+++ src/app/services/firestorage.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { finalize } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirestorageService {
 
-  constructor() { }
+  constructor(private storage: AngularFireStorage) { }
+
+  uploadMedia(file: any, path: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const filePath = `${path}/${file.name}`;
+      const fileRef = this.storage.ref(filePath);
+      const task = fileRef.put(file);
+
+      task.snapshotChanges().pipe(finalize(() => {
+        fileRef.getDownloadURL().subscribe({
+          next: (url) => resolve(url),
+          error: (err) => reject(err)
+        });
+      })).subscribe({error: (err) => 
+        reject(err)
+      });
+    });
+  }
+
 }
