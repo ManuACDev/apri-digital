927a63928571eef29cf53fb6da2d5a67d523e7a0
ManuACDev <malamocres@gmail.com>
2024-09-24 18:53:35 +0200

    Firestorage Service API

--- src/app/services/firestorage.service.ts
+++ src/app/services/firestorage.service.ts
@@ -1,28 +1,40 @@
 import { Injectable } from '@angular/core';
-import { AngularFireStorage } from '@angular/fire/compat/storage';
-import { finalize } from 'rxjs';
+import { Storage, ref, uploadBytesResumable, getDownloadURL } from '@angular/fire/storage';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirestorageService {
 
-  constructor(private storage: AngularFireStorage) { }
+  constructor(private storage: Storage) { }
 
-  uploadMedia(file: any, path: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-      const filePath = `${path}/${file.name}`;
-      const fileRef = this.storage.ref(filePath);
-      const task = fileRef.put(file);
+  uploadMedia(file: any, path: string): Observable<string> {
+    const filePath = `${path}/${file.name}`;
+    const storageRef = ref(this.storage, filePath);
+    const uploadTask = uploadBytesResumable(storageRef, file);
 
-      task.snapshotChanges().pipe(finalize(() => {
-        fileRef.getDownloadURL().subscribe({
-          next: (url) => resolve(url),
-          error: (err) => reject(err)
-        });
-      })).subscribe({error: (err) => 
-        reject(err)
-      });
+    return new Observable<string>((observer) => {
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Subida en progreso: ${progress}%`);
+        },
+        (error) => {
+          console.error("Error al subir el archivo:", error);
+          observer.error("Error al subir el archivo. Por favor, intenta de nuevo.");
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            observer.next(downloadURL);
+            observer.complete();
+          }).catch((error) => {
+            console.error("Error al obtener la URL de descarga:", error);
+            observer.error("Error al obtener la URL de descarga.");
+          });
+        }
+      );
     });
   }
 
