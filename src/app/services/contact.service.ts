6e765ce36269e0576059be34ce6a5360a3728d61
ManuACDev <malamocres@gmail.com>
2024-10-07 19:50:37 +0200

    Método Send Contact Form

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Functions, httpsCallable } from '@angular/fire/functions';
+import { from, lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  constructor() { }
+  constructor(private functions: Functions) { }
+
+  async sendContactForm(contactData: { name: string, email: string, message: string }) {
+    try {
+      const sendContactEmail = httpsCallable(this.functions, 'sendContactEmail');
+      const response = await lastValueFrom(from(sendContactEmail(contactData)));
+      return response;
+    } catch (error) {
+      console.error('Error al enviar el formulario de contacto:', error);
+      throw error;
+    }
+  }
 }
