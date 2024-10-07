16f45d62392778da0a076b6053532e962d4eefdc
ManuACDev <malamocres@gmail.com>
2024-10-07 19:57:27 +0200

    Function Send Contact Email

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,39 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const admin = require("firebase-admin");
+const nodemailer = require("nodemailer");
+const cors = require("cors")({ origin: true });
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Inicializar Firebase Admin
+admin.initializeApp();
+
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: "apridigitals@gmail.com",  // Tu correo de Gmail
+    pass: "ejch guga idkz kbfo", // Contraseña generada de la app
+  },
+});
+
+exports.sendContactEmail = onRequest(async (req, res) => {
+  cors(req, res, async ()=> {
+    const { name, email, message } = req.body.data;
+
+    const mailOptions = {
+      from: "apridigitals@gmail.com",
+      replyTo: email, 
+      to: "apridigitals@gmail.com",
+      subject: `Nuevo mensaje de ${name}`,
+      text: `Nombre: ${name}\nCorreo: ${email}\n\nMensaje:\n${message}`
+    };
+
+    try {      
+      await transporter.sendMail(mailOptions);
+      return res.status(200).send({ data: { success: true } });
+    } catch (error) {
+      console.error("Error al enviar el correo: ", error);
+      return res.status(500).send({ data: { success: false, error: error.toString() } });
+    }
+  })
+});
