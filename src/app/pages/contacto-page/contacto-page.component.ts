6ae6c8e0f35219189ede2d9b54bad43e6e34d53f
ManuACDev <malamocres@gmail.com>
2024-10-07 19:56:57 +0200

    Contacto Page

--- src/app/pages/contacto-page/contacto-page.component.ts
+++ src/app/pages/contacto-page/contacto-page.component.ts
@@ -3,6 +3,7 @@ import { TitleComponent } from "../../components/title/title.component";
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { InteractionService } from '../../services/interaction.service';
+import { ContactService } from '../../services/contact.service';
 
 @Component({
   selector: 'app-contacto-page',
@@ -16,7 +17,7 @@ export class ContactoPageComponent implements OnInit {
   contactForm: FormGroup;
   submitted = false;
 
-  constructor(private fb: FormBuilder, public interaction: InteractionService) {
+  constructor(private fb: FormBuilder, public interaction: InteractionService, private contact: ContactService) {
     this.contactForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
@@ -30,14 +31,24 @@ export class ContactoPageComponent implements OnInit {
 
   onSubmit() {
     this.submitted = true;
-    if (this.contactForm.valid) {
-      console.log('Formulario enviado', this.contactForm.value);
     
-      this.interaction.showSuccessMessage("¡Gracias por tu mensaje! Nos pondremos en contacto contigo pronto.");
+    if (this.contactForm.valid) {
+      this.interaction.showSuccessMessage("Formulario enviado correctamente.");      
+      this.contact.sendContactForm(this.contactForm.value).then(() => {
+        this.interaction.showSuccessMessage("¡Gracias por tu mensaje! Nos pondremos en contacto contigo pronto.");
+        setTimeout(() => {
+          this.contactForm.reset();
+          this.submitted = false;
+          this.interaction.clearMessages();
+        }, 5000);
+      }).catch((error) => {
+        console.error("Error al enviar el formulario de contacto:", error);
+        this.interaction.showErrorMessage("Error al enviar el mensaje");
+      });
     } else {
       this.interaction.showErrorMessage("Por favor, completa todos los campos correctamente.");
     }
-  }
+  }  
 
   get f() { return this.contactForm.controls; }
 }
